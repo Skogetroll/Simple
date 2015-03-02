da0458c7aef0d25cbf867705e9405562e46baad5
Mikhail Stepkin <mikhail.s@ramotion.com>
2015-03-02 16:31:59 +0300

    Initial Commit

---
+++ Simple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple
+//
+//  Created by Mikhail Stepkin on 3/2/15.
+//  Copyright (c) 2015 Mikhail Stepkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
